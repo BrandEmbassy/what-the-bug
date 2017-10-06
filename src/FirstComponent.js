d12922862e2f83b0880fa68e918a038583dd0201
Peter Dzuruš <dzurus.peter@gmail.com>
2017-10-06 13:24:14 +0200

    material-design added

---
+++ src/FirstComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class FirstComponent extends Component {
+  render() {
+    return (
+      <RaisedButton label="Default" ></RaisedButton>
+    );
+  }
+}
